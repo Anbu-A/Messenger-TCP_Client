c9b5996b87460b24b122e2a4c0d0057d3e5b9be6
Anbu-A <82471581+Anbu-A@users.noreply.github.com>
2021-04-26 13:42:46 +0200

    first (more or less) working version

---
+++ Socket_Chat_Client/Socket_Chat_Client/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Extras for this Project
+using System.Net;
+using System.Net.Sockets;
+
+namespace Socket_Chat_Client
+{
+    public partial class Form1 : Form
+    {
+        String ip_address;
+        int port;
+        bool error_on_connection;
+        IPEndPoint ipe;
+        String user_name;
+        Socket socket = null;
+        Form form = null;
+        public static Form1 instance;
+
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Später weg machen nur weil faul
+            textBox1.Text = "127.0.0.1";
+            textBox2.Text = "9999";
+            textBox3.Text = "achi";
+
+            ip_address = textBox1.Text;
+            port = Int32.Parse(textBox2.Text);
+            user_name = textBox3.Text;
+
+
+            error_on_connection = false;
+            // Setup for the connection
+            try
+            {
+                ipe = new IPEndPoint(IPAddress.Parse(ip_address), port);
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+            catch (System.FormatException exc)
+            {
+                error_on_connection = true;
+                Console.WriteLine(exc.Message);
+                MessageBox.Show(exc.Message);
+            }
+            catch (System.ArgumentOutOfRangeException exc)
+            {
+                error_on_connection = true;
+                Console.WriteLine(exc.Message);
+                MessageBox.Show(exc.Message);
+            }
+
+            // Connection 
+            try
+            {
+                if (socket != null)
+                {
+                    socket.Connect(ipe);
+                }
+            }
+            catch (System.Net.Sockets.SocketException exc)
+            {
+                error_on_connection = true;
+                Console.WriteLine(exc.Message);
+                MessageBox.Show(exc.Message);
+            }
+            catch (System.NullReferenceException exc)
+            {
+                error_on_connection = true;
+                Console.WriteLine(exc.Message);
+                MessageBox.Show(exc.Message);
+            }
+
+            if (error_on_connection)
+            {
+                MessageBox.Show("Something went wrong please enter your details again");
+            }
+            else
+            {
+                // Switch from Form1 to Form2
+                this.Hide();
+                if (form == null)
+                {
+                    form = new Form2(socket, user_name);
+                }
+                form.Show();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Socket_Chat_Client/Socket_Chat_Client/Form2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Extras for this Project
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Socket_Chat_Client
+{
+    public partial class Form2 : Form
+    {
+        String user_name;
+        Socket socket;
+        String msg = "";
+        Thread recv_thread;
+        Form form = null;
+        const String DC_COMMAND = "/dc";
+
+
+        public Form2(Socket soc, String usr_n)
+        {
+            InitializeComponent();
+            socket = soc;
+            user_name = usr_n;
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            send_message(user_name);
+
+            recv_thread = new Thread(receive_message);
+            recv_thread.Start();
+        }
+
+
+        private delegate void SafeCallDelegate(string text);
+        private void write_text_safe(string text)
+        {
+            if (textBox2.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(write_text_safe);
+                textBox2.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                textBox2.Text = text;
+            }
+        }
+
+
+        private void receive_message()
+        {
+            byte[] buffer = new byte[1024];
+            int byte_received = 0;
+            String msg = "";
+            bool error = false;
+
+            while (true)
+            {
+                try
+                {
+                    byte_received = socket.Receive(buffer);
+                }
+                catch (System.Net.Sockets.SocketException exc)
+                {
+                    MessageBox.Show(exc.Message);
+                    error = true;
+                }
+                catch (System.ObjectDisposedException exc)
+                {
+                    MessageBox.Show(exc.Message);
+                    error = true;
+                }
+
+                if (error)
+                {
+                    //Invoke(new MethodInvoker(switch_window));              
+                    switch_window();
+                }
+                else
+                {
+                    if (byte_received != 0)
+                    {
+                        msg += Encoding.ASCII.GetString(buffer, 0, byte_received) + Environment.NewLine;
+                        write_text_safe(msg);
+                    }
+                }
+            }
+        }
+
+
+        private void send_message(String msg)
+        {
+            byte[] msg_byte_format = Encoding.ASCII.GetBytes(msg);
+            socket.Send(msg_byte_format);
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // To prevent the "ding" sound when pressing enter
+                // Credits: https://stackoverflow.com/questions/6290967/stop-the-ding-when-pressing-enter , user: Mawardy
+                e.Handled = true;
+
+                msg = textBox1.Text;
+                textBox2.Text += $"[{user_name}]: {msg}" + Environment.NewLine;
+
+                if (msg == DC_COMMAND)
+                {
+                    switch_window();
+                }
+                else
+                {
+                    send_message(msg);
+
+                    msg = "";
+                    textBox1.Text = "";
+                }
+            }
+        }
+
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            close_client_con();
+            Application.Exit();
+        }
+
+
+        private void switch_window()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(switch_window));
+            }
+            else
+            {
+                // Switch from Form2 to Form1
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(this.Hide));
+                }
+                else
+                {
+                    this.Hide();
+                }
+
+                if (form == null)
+                {
+                    form = new Form1();
+                }
+                form.Show();
+                close_client_con();
+            }
+
+        }
+
+
+        private void close_client_con()
+        {
+            try
+            {
+                if (socket != null)
+                    socket.Shutdown(SocketShutdown.Both);
+            }
+            catch (System.ObjectDisposedException)
+            {
+                Console.WriteLine("Already closed");
+            }
+            catch (System.Net.Sockets.SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (recv_thread != null)
+                    recv_thread.Abort();
+
+                if (socket != null)
+                    socket.Close();
+            }
+        }
+    }
+}
